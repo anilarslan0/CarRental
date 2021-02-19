ae229c5ba194d30df661da23341504bed75ff48d
Anıl ARSLAN <anlarsln0@gmail.com>
2021-02-19 17:49:47 +0300

    Validation Added

--- ReCapProject/Business/Concrete/CarManager.cs
+++ ReCapProject/Business/Concrete/CarManager.cs
@@ -1,5 +1,8 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcern.Validation;
 using Core.Utilities.Results.Abstract;
 using Core.Utilities.Results.Concrete;
 using DataAccess.Abstract;
@@ -20,45 +23,40 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            if (car.CarName.Length>=2 && car.DailyPrice>0)
-            {
-                _carDal.Add(car);
-                return new SuccessResult(Messages.CarAdded);
-               
-            }
-            else
-            {
-                return new SuccessResult(Messages.CarNameInvalid);
-            }
+            ValidationTool.Validate(new CarValidator(), car);
+
+            _carDal.Add(car);
+            return new SuccessResult(Messages.CarAdded);
         }
 
         public IResult Delete(Car car)
         {
             _carDal.Delete(car);
             return new SuccessResult();
-            
+
         }
 
         public IDataResult<List<Car>> GetAll()
         {
-            return new SuccessDataResult<List<Car>>(_carDal.GetAll(),Messages.CarListed);
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(), Messages.CarListed);
         }
 
         public IDataResult<List<Car>> GetAllByBrandId(int Id)
         {
-            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c=>c.BrandId==Id));
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.BrandId == Id));
         }
 
         public IDataResult<List<Car>> GetAllByColorId(int Id)
         {
-            return new SuccessDataResult<List<Car>>( _carDal.GetAll(c => c.ColorId == Id));
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.ColorId == Id));
         }
 
         public IDataResult<Car> GetbyCarId(int carId)
         {
-            return new SuccessDataResult<Car>( _carDal.Get(c=>c.CarId==carId));
+            return new SuccessDataResult<Car>(_carDal.Get(c => c.CarId == carId));
         }
 
         public IDataResult<List<CarDetailDto>> GetCarDetail()
@@ -70,9 +68,9 @@ namespace Business.Concrete
         {
             _carDal.Update(car);
             return new SuccessResult();
-            
+
         }
 
-     
+
     }
 }
---
+++ ReCapProject/Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator:AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(p => p.CarName).NotEmpty();
+            RuleFor(p => p.CarName).MinimumLength(2);
+            RuleFor(p => p.DailyPrice).GreaterThan(0);
+            RuleFor(p => p.DailyPrice).GreaterThanOrEqualTo(100000).When(p => p.CarName == "Mercedes").WithMessage("Mercedes Daha pahalı olmalı.");
+           // RuleFor(p => p.CarName).Must(StartWithUpper);
+        }
+
+        //private bool StartWithUpper(string arg)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
